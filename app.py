f186ec150b53bfa13067b0a7ba9f2a361ea00ecb
tyde7 <32335591+tyde7@users.noreply.github.com>
2019-05-11 19:58:10 +0800

    初始Project.

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask,  render_template, request
+from fdb_tool.fdb_test import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/query', methods=['GET', 'POST'] )
+def query():
+    if request.method == 'GET':
+        return render_template('query.html')
+    if request.method == 'POST':
+        city = request.form.get('city')
+        name = request.form.get('name')
+        items = get_all_concerts(db, (name, city))
+        return render_template('query_result.html',items=items)
+
+@app.route('/order', methods=['POST'])
+def order():
+    city = request.json['city']
+    name = request.json['name']
+    month = request.json['month']
+    userid = request.json['userid']
+    signup(db, "s"+userid,(name, city, month))
+    return '', 200
+
+@app.route('/user', methods=['GET','POST'])
+def userq():
+    if request.method == 'GET':
+        return render_template('userticket.html')
+    if request.method == 'POST':
+        userid = request.form.get('userid')
+        tickets = get_all_attends(db, ("s"+userid,))
+        return render_template('userticket_result.html',tickets=tickets,userid=userid)
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ fdb_tool/fdb_test.py
@@ -0,0 +1,163 @@
+import itertools
+import traceback
+
+import fdb
+import fdb.tuple
+
+fdb.api_version(600)
+
+####################################
+##        Initialization          ##
+####################################
+
+# Data model:
+# ('attends', student, class) = ''
+# ('class', class_name) = seats_left
+
+db = fdb.open()
+scheduling = fdb.directory.create_or_open(db, ('scheduling',))
+course = scheduling['concerts']
+attends = scheduling['attends']
+
+
+@fdb.transactional
+def add_class(tr, c):
+    tr[course.pack(c)] = fdb.tuple.pack((100,))
+
+
+# Generate 1,620620 classes like '9:00 chem for dummies'
+# Generate 792 names like '9:00 chem for dummies'
+levels = ['张惠妹', '林俊杰', '周杰伦', '五月天', '杨坤', '王力宏',
+          'A-Lin', '苏打绿', '陈奕迅', '张惠妹', '孙燕姿']
+types = ['上海', '南京', '北京', '武汉', '杭州', '郑州',
+         '西安', '深圳', '重庆', '成都', '广州', '福州',
+         '厦门', '长沙', '大连', '青岛', '常州', '合肥']
+from random import sample
+
+times = ['%d月' % h for h in range(1, 13)]
+class_combos = itertools.product(levels, types, sorted(sample(times, 4)))
+# class_names = [''.join(tup) + '演唱会' for tup in class_combos]
+class_names = [tup for tup in class_combos]
+
+
+@fdb.transactional
+def init(tr):
+    del tr[scheduling.range(())]  # Clear the directory
+    for class_name in class_names:
+        add_class(tr, class_name)
+
+
+####################################
+##  Class Scheduling Functions    ##
+####################################
+
+
+@fdb.transactional
+def available_classes(tr):
+    return [course.unpack(k)[0] for k, v in tr[course.range(())]
+            if fdb.tuple.unpack(v)[0]]
+
+
+@fdb.transactional
+def signup(tr, s, c):
+    rec = attends.pack((s, c))
+    if tr[rec].present(): return  # already signed up
+
+    seats_left = fdb.tuple.unpack(tr[course.pack(c)])[0]
+    if not seats_left: raise Exception('No remaining seats')
+
+    classes = tr[attends.range((s,))]
+    if len(list(classes)) == 5: raise Exception('Bought too many tickets one time.')
+
+    tr[course.pack(c)] = fdb.tuple.pack((seats_left - 1,))
+    tr[rec] = b''
+
+
+@fdb.transactional
+def get_keys(tr, s):
+    pairs = tr[attends.range((s,))]
+    return [attends.unpack(k)[-1] for k, v in pairs]
+
+
+@fdb.transactional
+def get_all_attends(tr,conditions=()):
+    pairs = tr[attends.range(conditions)]
+    return [attends.unpack(k) for k, v in pairs]
+
+
+@fdb.transactional
+def get_all_concerts(tr, conditions=()):
+    pairs = tr[course.range(conditions)]
+    return [(course.unpack(k), fdb.tuple.unpack(v)[0]) for k, v in pairs]
+
+
+@fdb.transactional
+def drop(tr, s, c):
+    rec = attends.pack((s, c))
+    if not tr[rec].present(): return  # not taking this class
+    tr[course.pack(c)]= fdb.tuple.pack((fdb.tuple.unpack(tr[course.pack(c)])[0] + 1,))
+    del tr[rec]
+
+
+@fdb.transactional
+def switch(tr, s, old_c, new_c):
+    drop(tr, s, old_c)
+    signup(tr, s, new_c)
+
+
+####################################
+##           Testing              ##
+####################################
+
+import random
+import threading
+
+
+def indecisive_student(i, ops):
+    student_ID = 's{:d}'.format(i)
+    all_classes = class_names
+    my_classes = []
+
+    for i in range(ops):
+        class_count = len(my_classes)
+        moods = []
+        if class_count: moods.extend(['drop', 'switch'])
+        if class_count < 5: moods.append('add')
+        mood = random.choice(moods)
+
+        try:
+            if not all_classes:
+                all_classes = available_classes(db)
+            if mood == 'add':
+                c = random.choice(all_classes)
+                signup(db, student_ID, c)
+                my_classes.append(c)
+            elif mood == 'drop':
+                c = random.choice(my_classes)
+                drop(db, student_ID, c)
+                my_classes.remove(c)
+            elif mood == 'switch':
+                old_c = random.choice(my_classes)
+                new_c = random.choice(all_classes)
+                switch(db, student_ID, old_c, new_c)
+                my_classes.remove(old_c)
+                my_classes.append(new_c)
+        except Exception as e:
+            traceback.print_exc()
+            print("Need to recheck available classes.")
+            all_classes = []
+
+
+def run(students, ops_per_student):
+    threads = [
+        threading.Thread(target=indecisive_student, args=(i, ops_per_student))
+        for i in range(students)]
+    for thr in threads: thr.start()
+    for thr in threads: thr.join()
+    print("Ran {} transactions".format(students * ops_per_student))
+
+
+if __name__ == "__main__":
+    init(db)
+    print("initialized")
+    run(10, 100)
